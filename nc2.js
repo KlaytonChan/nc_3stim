c859f209e77ace8b5d6d6a66e4c51369a31aa8cf
KlaytonChan <9bp4nbzf49@privaterelay.appleid.com>
2026-06-03 15:32:38 +0800

    Update nc2.js

--- nc2.js
+++ nc2.js
@@ -1818,7 +1818,7 @@ function thankRoutineBegin(snapshot) {
           method: 'POST',
           headers: { 'Content-Type': 'application/json', 'Accept': '*/*' },
           body: JSON.stringify({
-            experimentID: 'Pyz0Uh6L3iCs',
+            experimentID: 'zf6EpbAf02Kb',
             filename: filename,
             data: csvData
           })
